f1d2fb340e532bb16097884934d8861971daed4d
anthony <theanthonyarellano@gmail.com>
2022-04-11 18:37:56 -0400

    loaded in fonts and altered css

--- react-app/src/components/Banner/index.js
+++ react-app/src/components/Banner/index.js
@@ -1,14 +1,25 @@
+import './style/banner.css';
+
 export const Banner = () => {
     return (
         <div className="banner-container">
-            <div>
-
+            <div className="banner-logo-container">
+                <div>
+                <img
+                    style={{height: "28px", width: "16px"}}
+                    src={require("./style/wanderer-logo.png").default} alt=""></img>
+                </div>
+                <div className='banner-logo-text-container'>
+                    <p className='banner-logo-text'>wanderer</p>
+                </div>
             </div>
-            <div>
 
-            </div>
             <div>
-                
+                {/* Search */}
+            </div>
+
+            <div>
+                {/* Login + other buttons */}
             </div>
         </div>
     )
