7655d0babaedb4a201ba006d4178a8f361db8bd5
anthony <theanthonyarellano@gmail.com>
2022-04-12 18:40:09 -0400

    imported images into component

--- react-app/src/components/ListingNavBar/Amenities.js
+++ react-app/src/components/ListingNavBar/Amenities.js
@@ -7,40 +7,51 @@ const Amenities = () => {
             <div className="amenities-grid-container">
                 <div>
                     <input type="checkbox"></input>
-                    <img src={require('./style/images/svgexport-20.svg').default}></img>
+                    <img src={require('./style/images/ac.png').default}></img>
                 </div>
                 <div>
                     <input type="checkbox"></input>
+                    <img src={require('./style/images/bbq.png').default}></img>
                 </div>
                 <div>
                     <input type="checkbox"></input>
+                    <img src={require('./style/images/dryer.png').default}></img>
                 </div>
                 <div>
                     <input type="checkbox"></input>
+                    <img src={require('./style/images/washer.png').default}></img>
                 </div>
                 <div>
                     <input type="checkbox"></input>
+                    <img src={require('./style/images/fridge.png').default}></img>
                 </div>
                 <div>
                     <input type="checkbox"></input>
+                    <img src={require('./style/images/hairdryer.png').default}></img>
                 </div>
                 <div>
                     <input type="checkbox"></input>
+                    <img src={require('./style/images/kitchen.png').default}></img>
                 </div>
                 <div>
                     <input type="checkbox"></input>
+                    <img src={require('./style/images/parking.png').default}></img>
                 </div>
                 <div>
                     <input type="checkbox"></input>
+                    <img src={require('./style/images/pool.png').default}></img>
                 </div>
                 <div>
                     <input type="checkbox"></input>
+                    <img src={require('./style/images/stove.png').default}></img>
                 </div>
                 <div>
                     <input type="checkbox"></input>
+                    <img src={require('./style/images/tv.png').default}></img>
                 </div>
                 <div>
                     <input type="checkbox"></input>
+                    <img src={require('./style/images/wifi.png').default}></img>
                 </div>
             </div>
         </div>
