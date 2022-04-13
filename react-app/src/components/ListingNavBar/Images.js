da25db2b52829ee3c9182705327df9cdaae09323
anthony <theanthonyarellano@gmail.com>
2022-04-12 22:03:48 -0400

    idk how I got here but draggable photos

--- react-app/src/components/ListingNavBar/Images.js
+++ react-app/src/components/ListingNavBar/Images.js
@@ -1,7 +1,85 @@
-const Images = () => {
+import { useDropzone } from 'react-dropzone';
+import { useEffect } from 'react';
+import Photo from './Photo';
+import './style/images.css';
+import Gallery from "react-photo-gallery";
+import {arrayMoveImmutable} from "array-move";
+import { SortableContainer, SortableElement } from "react-sortable-hoc";
+
+const SortablePhoto = SortableElement(item => <Photo {...item} />);
+const SortableGallery = SortableContainer(({ items }) => (
+    <Gallery photos={items} renderImage={props => <SortablePhoto {...props} />} />
+
+));
+
+const Images = ({ imagesFuncs }) => {
+
+    const {
+        files, setFiles
+    } = imagesFuncs;
+    console.log(files);
+    useEffect(() => {
+        // Make sure to revoke the data uris to avoid memory leaks
+        // files.forEach(file => URL.revokeObjectURL(file.preview));
+        files?.forEach(file => URL.createObjectURL(file))
+
+    }, [files]);
+
+    const onSortEnd = ({ oldIndex, newIndex }) => {
+        setFiles(arrayMoveImmutable(files, oldIndex, newIndex));
+    };
+
+    const baseStyle = {
+        flex: 1,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: '20px',
+        borderWidth: 2,
+        borderRadius: 2,
+        borderColor: '#eeeeee',
+        borderStyle: 'dashed',
+        backgroundColor: '#fafafa',
+        color: '#bdbdbd',
+        outline: 'none',
+        transition: 'border .24s ease-in-out'
+    };
+
+    const thumbsContainer = {
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        marginTop: 16
+    };
+
+    const { getRootProps, getInputProps } = useDropzone({
+        accept: 'image/png',
+        onDrop: acceptedFiles => {
+            setFiles(acceptedFiles.map(file => Object.assign(file, {
+                preview: URL.createObjectURL(file)
+            })));
+        }
+    });
+
+    const photos = []
+    const thumbs = files?.map(file => (
+        photos.push({ src: file.preview, width: 3, height: 3})
+    ));
+
+
     return (
-        <p>Images</p>
-    )
+        <section className="container">
+            <div {...getRootProps({ style: baseStyle })}>
+                <input {...getInputProps()} />
+                <p>Drag 'n' drop for multiple files here, or click to select one file</p>
+            </div>
+            <aside style={thumbsContainer}>
+                <div>
+                    <SortableGallery style={{width: "500px"}} items={photos} onSortEnd={onSortEnd} axis={"xy"} />
+                </div>
+            </aside>
+        </section>
+    );
 };
 
 export default Images;
