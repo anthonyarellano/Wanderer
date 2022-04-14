81a9520f32831b2050be3ad5695cc170b785a327
anthony <theanthonyarellano@gmail.com>
2022-04-14 19:16:22 -0400

    minor styling on listing page css

--- react-app/src/components/ListingProfile/index.js
+++ react-app/src/components/ListingProfile/index.js
@@ -17,6 +17,18 @@ const ListingProfile = () => {
         dispatch(getImages(listingId))
     }, [])
 
+    const style1 = {
+        height: '100%', width: '100%', objectFit: "cover"
+    }
+
+    const style2 = {
+        height: '100%', width: '100%', objectFit: "cover", borderRadius: "0px 10px 0px 0px"
+    }
+
+    const style3 = {
+        height: '100%', width: '100%', objectFit: "cover", borderRadius: "0px 0px 10px 0px"
+    }
+
     return (
         <div className='listing-profile-container'>
             <div>
@@ -27,8 +39,8 @@ const ListingProfile = () => {
                     <img style={{width: '100%', height: '100%', borderRadius: "10px 0px 0px 10px"}} src={images[0]}></img>
                 </div>
                 <div className='listing-profile-secondary-images'>
-                    {secondaryImages?.map((url) => (
-                        <img className='image' style={{height: '100%', width: '100%', objectFit: "cover"}} src={url}></img>
+                    {secondaryImages?.map((url, i) => (
+                        <img className='image' style={i == 1 ? style2 : i == 3 ? style3 : style1} src={url}></img>
                     ))}
                 </div>
             </div>
