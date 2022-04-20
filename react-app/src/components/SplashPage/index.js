93861c79eb4dfea2478daeb1c24dcdb5d23d8c03
anthony <theanthonyarellano@gmail.com>
2022-04-20 19:05:10 -0400

    splash page done

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -1,12 +1,17 @@
+import { useHistory } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import './style/splash-page.css';
 
 const SplashPage = () => {
+    const history = useHistory();
+
     return (
         <div className="splash-page-container">
             <div className='splash-page-title unselectable'>wanderer</div>
             <div className='splash-page-desc unselectable'>a hub for wanderers and galabouts to find their next adventure</div>
-            <div className='splash-page-subtitle unselectable'>enter</div>
+            <div
+                onClick={() => history.push('/home')}
+                className='splash-page-subtitle unselectable'>enter</div>
             <div
                 style={{
                     marginTop: "1%"
