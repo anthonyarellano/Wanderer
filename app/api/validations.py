e0779f54be816ef20c90675df456b365a1a2d456
anthony <theanthonyarellano@gmail.com>
2022-04-24 17:52:49 -0400

    added type checking to listing creation validations

--- app/api/validations.py
+++ app/api/validations.py
@@ -83,30 +83,80 @@ def verify_listing_values(listing):
     errors = []
     if listing['owner_id'] <= 0:
         errors.append('Invalid owner_id')
-    if len(listing['title']) <= 0 or len(listing['title']) > 255:
-        errors.append('title must be between 1 and 255 characters.')
-    if listing['bed_number'] <= 0 or listing['bed_number'] > 2147483647:
-        errors.append('bed_number must be greater than 0 and within postgreSQL integer range.')
-    if listing['bath_number'] <= 0 or listing['bed_number'] > 2147483647:
-         errors.append('bath_number must be greater than 0 and within postgreSQL integer range.')
-    if listing['bedroom_number'] <= 0 or listing['bed_number'] > 2147483647:
-         errors.append('bedroom_number must be greater than 0 and within postgreSQL integer range.')
-    if listing['maximum_guests'] <= 0 or listing['bed_number'] > 2147483647:
-         errors.append('maximum_guests must be greater than 0 and within postgreSQL integer range.')
-    if listing['price'] <= 0 or listing['price'] > 2147483647:
-         errors.append('price must be greater than 0 and within postgreSQL integer range.')
-    if listing['latitude'] > 90 or listing['latitude'] < -90:
-        errors.append('Invalid latitude.')
-    if listing['longitude'] > 180 or listing['longitude'] < -180:
-        errors.append('Invalid longitude.')
-    if len(listing['city']) <= 0 or len(listing['city']) > 255:
-        errors.append('city must be greater than 0 and less than 255 characters.')
-    if len(listing['address']) <= 0 or len(listing['address']) > 255:
-        errors.append('address must be greater than 0 and less than 255 characters.')
-    if len(listing['state']) <= 0 or len(listing['state']) > 255:
-        errors.append('state must be greater than 0 and less than 255 characters.')
-    if len(listing['description']) <= 0 or len(listing['description']) > 2000:
-        errors.append('description must be greater than 0 and less than 2000 characters.')
+
+    if type(listing['title']) is not str:
+        errors.append('title must be a string.')
+    if type(listing['title']) is str:
+        if len(listing['title']) <= 0 or len(listing['title']) > 255:
+            errors.append('title must be between 1 and 255 characters.')
+
+    if type(listing['bed_number']) is not int:
+        errors.append('bed_number must be an integer.')
+    if type(listing['bed_number']) is int:
+        if listing['bed_number'] <= 0 or listing['bed_number'] > 2147483647:
+            errors.append('bed_number must be greater than 0 and within postgreSQL integer range.')
+
+    if type(listing['bath_number']) is not int:
+        errors.append('bath_number must be an integer.')
+    if type(listing['bath_number']) is int:
+        if listing['bath_number'] <= 0 or listing['bath_number'] > 2147483647:
+            errors.append('bath_number must be greater than 0 and within postgreSQL integer range.')
+
+    if type(listing['bedroom_number']) is not int:
+        errors.append('bedroom_number must be an integer.')
+    if type(listing['bedroom_number']) is int:
+        if listing['bedroom_number'] <= 0 or listing['bedroom_number'] > 2147483647:
+            errors.append('bedroom_number must be greater than 0 and within postgreSQL integer range.')
+
+    if type(listing['maximum_guests']) is not int:
+        errors.append('maximum_guests must be an integer.')
+    if type(listing['maximum_guests']) is int:
+        if listing['maximum_guests'] <= 0 or listing['maximum_guests'] > 2147483647:
+            errors.append('maximum_guests must be greater than 0 and within postgreSQL integer range.')
+
+    if type(listing['price']) is not int:
+        errors.append('price must be an integer.')
+    if type(listing['price']) is int:
+        if listing['price'] <= 0 or listing['price'] > 2147483647:
+            errors.append('price must be greater than 0 and within postgreSQL integer range.')
+
+    if type(listing['latitude']) is not float:
+        errors.append('latitude must be a floating point integer.')
+    if type(listing['latitude']) is float:
+        if listing['latitude'] > 90 or listing['latitude'] < -90:
+            errors.append('Invalid latitude.')
+
+    if type(listing['longitude']) is not float:
+        errors.append('longitude must be a floating point integer.')
+    if type(listing['longitude']) is float:
+        if listing['longitude'] > 180 or listing['longitude'] < -180:
+            errors.append('Invalid longitude.')
+
+    if type(listing['city']) is not str:
+        errors.append('city must be an string.')
+    if type(listing['city']) is str:
+        if len(listing['city']) <= 0 or len(listing['city']) > 255:
+            errors.append('city must be greater than 0 and less than 255 characters.')
+
+    if type(listing['address']) is not str:
+        errors.append('address must be an string.')
+    if type(listing['address']) is str:
+        if len(listing['address']) <= 0 or len(listing['address']) > 255:
+            errors.append('address must be greater than 0 and less than 255 characters.')
+
+    if type(listing['state']) is not str:
+        errors.append('state must be an string.')
+    if type(listing['state']) is str:
+        if len(listing['state']) <= 0 or len(listing['state']) > 255:
+            errors.append('state must be greater than 0 and less than 255 characters.')
+
+    if type(listing['description']) is not str:
+        errors.append('description must be an string.')
+    if type(listing['description']) is str:
+        if len(listing['description']) <= 0 or len(listing['description']) > 2000:
+            errors.append('description must be greater than 0 and less than 2000 characters.')
+
+
     if type(listing['wifi_avail']) is not bool:
         errors.append('wifi_avail must be a boolean value.')
     if type(listing['tv_avail']) is not bool:
@@ -131,6 +181,7 @@ def verify_listing_values(listing):
         errors.append('stove_avail must be a boolean value.')
     if type(listing['pool_avail']) is not bool:
         errors.append('pool_avail must be a boolean value.')
+
     if type(listing['check_in']) is not str:
         errors.append('check_in must be a string formatted H:M')
     if type(listing['check_in']) is str:
@@ -145,6 +196,7 @@ def verify_listing_values(listing):
              time.strptime(listing['check_out'], '%H:%M')
         except ValueError:
             errors.append("Incorrect check_out format, should be H:M")
+            
     if type(listing['room_type_id']) is not int:
         errors.append('room_type_id must be an integer.')
     if type(listing['room_type_id']) is int:
